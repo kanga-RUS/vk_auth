8ea6653005ab701af81e9b1cc7b63b32ae9b5ac0
kanga-RUS <amatiushina@gmail.com>
2020-02-04 21:04:18 +0300

    Fixed detecting user id and passing it via session variable

--- home/views.py
+++ home/views.py
@@ -1,27 +1,34 @@
-import re
-
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from vk_auth.settings import vk
 
-USERID_PATTERN = r'(\d+)'
-
 
 class HomePageView(TemplateView):
     template_name = 'home/home.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        return super(HomePageView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if not self.request.user.is_anonymous:
+            self.request.session['user_vk_id'] = \
+            self.request.user.socialaccount_set.all()[0].extra_data['id']
+        return context
+
 
 def search(request):
     result = dict()
-    user_id = int(re.search(USERID_PATTERN, request.user.username).group(1))
+    user_id = request.session['user_vk_id']
+
     if request.method == 'GET':
         search_text = request.GET['q']
         if search_text:
             result = vk.friends.search(user_id=user_id,
-                                        fields='first_name,last_name,domain',
-                                        count=1000,
-                                        q=search_text)
+                                       fields='first_name,last_name,domain',
+                                       count=1000,
+                                       q=search_text)
     context = {'result': result}
 
     return render(request, 'home/search_result.html', context=context)
